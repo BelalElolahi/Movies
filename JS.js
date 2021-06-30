3329a249f21d7795336504bccf1dabfb3cf938da
BelalElolahi <86521952+BelalElolahi@users.noreply.github.com>
2021-06-30 13:32:16 +0000

    update js file

--- JS.js
+++ JS.js
@@ -1,31 +1,26 @@
-alert("hello");
 var favoriteMovie = prompt("what is your favorite movie");
 if (favoriteMovie != null )
 {
-  if(favoriteMovie=="DORA") 
+  if(favoriteMovie=="DORA" || favoriteMovie =="Fast&Furious"||favoriteMovie =="The Quslizer2" ) 
 {
   var color1 = prompt("enter a color to change somthing");
+  
   document.getElementById("myH2").style.color = color1;
+  document.getElementById("myH3").style.color= color1;
+  document.getElementById("myH4").style.color = color1;
   document.write("<h2>"+ favoriteMovie +"<h2>");
   document.write("<h2>"+ color1 +"<h2>");
 
   document.getElementsByTagName('p').style.color = color1;
-}  
-else if (favoriteMovie =="Fast&Furious" ||favoriteMovie =="Fast and Furious ")
-{
-  var color1 = prompt("enter a color u need to change it ");
+} else{ 
+  var color1 = prompt("enter a color to change somthing");
+  
   document.getElementById("myH1").style.color = color1;
   document.write("<h2>"+ favoriteMovie +"<h2>");
   document.write("<h2>"+ color1 +"<h2>");
-} else if (favoriteMovie =="The Quslizer2")
-{
-  var color1 = prompt("enter a color u need to change it ");
-  document.getElementById("myH0").style.color = color1;
-  document.write("<h2>"+ favoriteMovie +"<h2>");
-  document.write("<h2>"+ color1 +"<h2>");
-} else 
-{
-  alert(" choose movie name form the website");
+
+
+  
 }
 
 }
